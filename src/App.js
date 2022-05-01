a7e90037a4963d8d6d6965f5e9e4d006b0417af6
Elakshi Shah <eshah@hmc.edu>
2022-04-30 21:43:01 -0700

    base form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <p>Policing Fairness Clinic</p>
+        <h1>Policing Fairness Clinic</h1>
+
+        {/* Access our Stuff at these Links */}
+        {/* Insert Link to Poster */}
+        <div id={"Downloads"}>
+            <a>Click here to Download our Poster</a>
+            <a href={"https://github.com/elakshi16/Policing-Fairness-Clinic-2021"}>Click here to Access our Code</a>
+            {/* Insert Link to Paper if we have it*/}
+            <a>Click here to Download our Paper</a>
+        </div>
+
+
     </div>
   );
 }
